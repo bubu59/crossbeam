11de14541ef67384979d70c976d71725cea47a64
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-06-12 00:24:13 +0800

    deleted_2_bags
    deleted 2 of the thingy in the bag struct

--- crossbeam-epoch/src/internal.rs
+++ crossbeam-epoch/src/internal.rs
@@ -56,7 +56,7 @@ use crate::sync::queue::Queue;
 
 /// Maximum number of objects a bag can contain.
 #[cfg(not(crossbeam_sanitize))]
-const MAX_OBJECTS: usize = 60;
+const MAX_OBJECTS: usize = 50;
 #[cfg(crossbeam_sanitize)]
 const MAX_OBJECTS: usize = 4;
 
@@ -113,16 +113,6 @@ impl Default for Bag {
         return Bag {
             len: 0,
             deferreds: [
-                Deferred::new(no_op_func),
-                Deferred::new(no_op_func),
-                Deferred::new(no_op_func),
-                Deferred::new(no_op_func),
-                Deferred::new(no_op_func),
-                Deferred::new(no_op_func),
-                Deferred::new(no_op_func),
-                Deferred::new(no_op_func),
-                Deferred::new(no_op_func),
-                Deferred::new(no_op_func),
                 Deferred::new(no_op_func),
                 Deferred::new(no_op_func),
                 Deferred::new(no_op_func),
